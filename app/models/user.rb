164814ed885af7888d37060d2a790c8d4405f846
paulzay <paulotieno2@gmail.com>
2020-08-12 17:52:51 +0300

    add create action in user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :email, presence: true, length: { minimum: 5, maximum: 30 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+      format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 8, maximum: 30 }
 end
